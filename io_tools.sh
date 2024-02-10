b5953e06a650a3d1479d756c202d3350a4b20b6d
LigmaLeng <96036878+LigmaLeng@users.noreply.github.com>
2024-02-10 14:50:44 +0000

    handling input parser

--- io_tools.sh
+++ io_tools.sh
@@ -124,8 +124,8 @@ display_clean() {
 
 display_cleave() {
   local fissure=$(echoes "\xE2\x94\x80" $((COLUMNS-2)))
-  # Cursor on transverse plane and save cursor state
-  printf "\x1B\x9B${TRANSVERSE};H\x1B7"
+  # Cursor on transverse plane
+  printf "\x1B\x9B${TRANSVERSE};H"
   # Grow fissure from from saggital plane laterally along transverse plane
   for ((i=0; i < SAGITTAL-1; i++)); {
     printf "\x1B\x9B$(( SAGITTAL - i))G\xE2\x95\x90"
@@ -133,10 +133,10 @@ display_cleave() {
     nap 0.004
   }
   # Ligate fissure
-  echoes "\xE2\x95\xAC\x1B8" 2 && nap 0.1
+  echoes "\xE2\x95\xAC\x1B\x9B${TRANSVERSE};H" 2 && nap 0.1
   # Pilot cleavage and swap ligatures
   printf "\xE2\x95\xA8%b" $fissure "\n"
-  printf "\xE2\x95\xA5%b" $fissure "\x1B8"
+  printf "\xE2\x95\xA5%b" $fissure "\x1B\x9B${TRANSVERSE};H"
   # Widen pilot cleave if lines are odd
   # A M B L: up  delete_line  down  insert_line
   ((LINES&1)) && printf "\x1B\x9B%s" A M B L A
@@ -218,19 +218,24 @@ ttin_parse() {
       case "${REPLY}" in
         $'\x7F'|$'\x08')
           ((str_idx)) && {
+            # ANSI backspace from current column
             printf "\x08"
+            # If at end of string, simple trim and space+backspace
             ((str_idx == ${#str})) && str=${str:0:-1} && printf "\x20\x08" || {
+              # Else print string tail, erase trailing char, and set cursor back
               printf "${str:${str_idx}}"
               printf "\x1B\x9B%s" X "$((${#str} - str_idx))D"
-              str=${str:0:${str_idx}}${str:${str_idx}}
+              # Trim char in middle of string
+              str=${str:0:${str_idx}}"${str:${str_idx}}"
             }
             str_idx--
           }
         ;;
         $'\x0A') echo "ent";; # stty set to icrnl; ENTER=Linefeed/newline
+        *)
+          printf ${REPLY}
+          str=$str"${REPLY}"
       esac
-      str=$str"$1"
-      printf "\x1B8$str"
     }
   }
 }
