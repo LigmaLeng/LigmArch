30d3f0adcff7019eba51290b0d4a50467865d055
LigmaLeng <96036878+LigmaLeng@users.noreply.github.com>
2024-03-27 01:06:44 +0000

    refactoring window context

--- io_tools.sh
+++ io_tools.sh
@@ -6,9 +6,11 @@
 readonly CACHE_DIR="${XDG_CACHE_HOME:=${HOME}/.cache/ligmarch.conf}"
 readonly TEMPLATE_DIR="${CTX_DIR}/options.conf"
 readonly READ_OPTS=(-rs -t 0.02)
-declare -i LINES COLUMNS TRANSVERSE SAGITTAL EXIT_STATE
-declare -a SETUP_OPTKEYS setup_opts_f KEYMAP_FILES SUPPORTED_LOCALES
-declare -A setup_opts user_opts
+declare -i LINES COLUMNS TRANSVERSE SAGITTAL
+declare -a SETOPT_KEYS SETOPT_KEYS_F KEYMAP_A LOCALES_A
+declare -a setopt_pairs_f win_ctx_a
+declare -A setopt_pairs
+declare -A win_ctx=(y '' x '' m '' n '' nref '' offset '' idx '')
 # https://archlinux.org/mirrorlist/all/https/
 # bootloader?
 # use swap?
@@ -26,11 +28,11 @@ die() {
 echoes()for((i=0;i++<$2;)){ printf $1;}
 
 nap() {
-  local IFS
   # Open file descriptor if doesn't exist pipe empty subshell
-  [[ -n "${nap_fd:-}" ]] || { exec {nap_fd}<> <(:);} 2>/dev/null
+  [[ -n "${nap_fd:-}" ]] || { exec {nap_fd}<> <(:);}
+  #[[ -n "${nap_fd:-}" ]] || { exec {nap_fd}<> <(:);} 2>/dev/null
   # Attempt to read from empty file descriptor indefinitely if no timeout given
-  read ${1:+-t "$1"} -u $nap_fd || :
+  read -t "${1:-0.001}" -u $nap_fd || :
 }
 
 set_console() {
@@ -74,84 +76,6 @@ test_size() {
   return 0
 }
 
-parse_files() {
-  local -i optsp=0
-  # Parse config template file
-  while read; do
-    case $REPLY in
-      # Section header
-      '['*)
-        # Trim brackets
-        : "${REPLY#[}"
-        SETUP_OPTKEYS+=("${_%]}")
-        # Keep track of longest optkey
-        ((optsp=${#SETUP_OPTKEYS[-1]}>optsp?${#SETUP_OPTKEYS[-1]}:$optsp))
-      ;;
-      value*)
-        setup_opts[${SETUP_OPTKEYS[-1]}]=${REPLY#*= }
-      ;;
-      list*)
-        read setup_opts[${SETUP_OPTKEYS[-1]}]
-        while read; do
-          [[ -z $REPLY ]] && break || {
-            setup_opts[${SETUP_OPTKEYS[-1]}]+=" ${REPLY//[[:space:]]}"
-          }
-        done
-      ;;
-    esac
-  done < "$TEMPLATE_DIR"
-  # Format optkey spacing for prints
-  for i in ${SETUP_OPTKEYS[@]}; do
-    setup_opts_f+=("${i/_/ }$(echoes '\x20' $((optsp-${#i}+3)))")
-  done
-  # Get kbd keymap files and parse supported locales
-  KEYMAP_FILES=($(localectl list-keymaps))
-  while read; do
-    SUPPORTED_LOCALES+=("$REPLY")
-  done < "/usr/share/i18n/SUPPORTED"
-}
-
-draw_window() {
-  local -i idx_y=${1:-1}
-  local -i idx_x=${2:-1}
-  local -i m=${3:-$LINES}
-  local -i n=${4:-$COLUMNS}
-  local -i offset=0
-  local horz=$(echoes '\xCD' $((n - 2)))
-  local vert="\xBA\x9B$((n-2))C\xBA"
-  #       Cursor origin and print top border
-  printf "\x9B${idx_y};${idx_x}H\xC9${horz}\xBB"
-  #       Print vertical borders on every line but first and last
-  for((;offset++<m-2;)){ printf "\x9B$((idx_y+offset));${idx_x}H${vert}";}
-  #       Print bottom border
-  printf "\x9B$((idx_y+m-1));${idx_x}H\xC8${horz}\xBC"
-  #       Bound scrolling region, bring cursor into window
-  printf "\x9B$((idx_y+1));%s" $((idx_y+m-2))r $((idx_x+1))H
-  #       Save cursor state
-  printf '\x1B7'
-}
-
-draw_menu() {
-  draw_window
-  for ((i=0;i<${#SETUP_OPTKEYS[@]};i++)); do
-    setup_opts_f[$i]="${setup_opts_f[$i]}${setup_opts[${SETUP_OPTKEYS[$i]}]}"
-  done
-  kb_nav setup_opts_f $((LINES-2)) $((COLUMNS-2))
-}
-
-draw_select() {
-  local -i optkey_idx=$1
-  local -a curs
-  curs_store curs
-  draw_window 2 $SAGITTAL $((LINES-2)) $((SAGITTAL-1))
-  case ${optkey_idx} in
-    0) : "KEYMAP_FILES";;
-    2) : "SUPPORTED_LOCALES";;
-  esac
-  kb_nav $_ $((LINES-4)) $((SAGITTAL-3))
-  curs_load curs
-}
-
 display_init() {
   get_console_size
   # 2J    Clear screen
@@ -188,29 +112,54 @@ display_cleave() {
 
 curs_store() {
   local -n ref=$1
-  local IFS='[;'
-  read -rs -d R -p $'\x9B6n' _ ref[0] ref[1] _
+  IFS='[;' read -rs -d R -p $'\x9B6n' _ ref[0] ref[1] _
 }
 
 curs_load() {
-  local -n ref=${1}
-  printf "\x9B${ref[0]};${ref[1]}H"
-  printf '\x1B7'
+  local -n ref=$1
+  printf "\x9B${ref[0]};${ref[1]}H\x1B7"
 }
 
 get_line() {
   local -i curs
-  local IFS='[;'
-  read -rs -d R -p $'\x9B6n' _ curs _ _
+  IFS='[;' read -rs -d R -p $'\x9B6n' _ curs _
   printf $curs
 }
 
+win_ctx_op(){
+  local -n w
+  local -a arr
+  case $1 in
+    'set')
+      w=win_ctx
+      read -d '' w[y] w[x] w[m] w[n] w[nref] w[offset] w[idx] <<< "${2//,/ }"
+    ;;
+    'push')
+      w=win_ctx
+      : "${w[y]},${w[x]},${w[m]},${w[n]},${w[nref]},${w[offset]},${w[idx]}"
+      win_ctx_a+=("$_")
+    ;;
+    'rebuild')
+      w=win_ctx_a
+      printf '\x9B%s' 2J r
+      for ((i=0;i<${#w[@]};i++)); do
+        read -d '' -a arr <<< "${w[$i]//,/ }"
+        draw_window ${arr[0]} "${arr[1]};${arr[2]}"
+      done
+    #;&
+    ;;
+    'pop')
+      unset win_ctx_a[-1]
+    ;;
+  esac
+}
+
 exit_prompt() {
-  ((EXIT_STATE)) && return
-  EXIT_STATE=1
+  [[ ${FUNCNAME[1]} == 'exit_prompt' ]] && return
   local exit_query='Abort setup process'
   local exit_opts=('(Y|y)es' '(N|n)o')
   local -a curs
+  win_ctx_op 'push'
   curs_store curs && display_cleave
   # Center cursor
   printf "\x9B$((TRANSVERSE-(LINES&1)));${SAGITTAL}H"
@@ -233,23 +182,97 @@ exit_prompt() {
       *) continue;;
     esac
   }
-  EXIT_STATE=0 && display_init && curs_load curs
+  win_ctx_op 'rebuild'
+  #printf '%s %s\n' ${win_ctx_test[@]@k}
+}
+
+parse_files() {
+  local -i optsp=0
+  # Parse config template file
+  while read; do
+    case $REPLY in
+      # Section header
+      '['*)
+        # Trim brackets
+        : "${REPLY#[}"
+        SETOPT_KEYS+=("${_%]}")
+        # Keep track of longest optkey
+        ((optsp=${#SETOPT_KEYS[-1]}>optsp?${#SETOPT_KEYS[-1]}:$optsp))
+      ;;
+      value*)
+        setopt_pairs[${SETOPT_KEYS[-1]}]=${REPLY#*= }
+      ;;
+      list*)
+        read setopt_pairs[${SETOPT_KEYS[-1]}]
+        while read; do
+          [[ -z $REPLY ]] && break || {
+            setopt_pairs[${SETOPT_KEYS[-1]}]+=" ${REPLY//[[:space:]]}"
+          }
+        done
+      ;;
+    esac
+  done < "$TEMPLATE_DIR"
+  # Format optkey spacing for prints
+  for i in ${SETOPT_KEYS[@]}; do
+    SETOPT_KEYS_F+=("${i/_/ }$(echoes '\x20' $((optsp-${#i}+3)))")
+  done
+  # Get kbd keymap files and parse supported locales
+  KEYMAP_A=($(localectl list-keymaps))
+  while read; do
+    LOCALES_A+=("$REPLY")
+  done < "/usr/share/i18n/SUPPORTED"
+  declare -r SETOPT_KEYS SETOPT_KEYS_F KEYMAP_A LOCALES_A
+}
+
+draw_window() {
+  local -n w=win_ctx
+  local -i offset=0
+  local horz=$(echoes '\xCD' $((w[n] - 2)))
+  local vert="\xBA\x9B$((w[n]-2))C\xBA"
+  #       Cursor origin and print top border
+  printf "\x9B${w[y]};${w[x]}H\xC9${horz}\xBB"
+  #       Print vertical borders on every line but first and last
+  for((;offset++<w[m]-2;)){ printf "\x9B$((w[y]+offset));${w[x]}H${vert}";}
+  #       Print bottom border
+  printf "\x9B$((w[y]+w[m]-1));${w[x]}H\xC8${horz}\xBC"
+  #       Bound scrolling region, bring cursor into window
+  printf "\x9B$((w[y]+1));%s" $((w[y]+w[m]-2))r $((w[x]+1))H
+  #       Save cursor state
+  printf '\x1B7'
+}
+
+draw_main() {
+  win_ctx_op 'set' "1,1,${LINES},${COLUMNS},setopt_pairs_f,0,0"
+  draw_window
+  for ((i=0;i<${#SETOPT_KEYS[@]};i++)); do
+    setopt_pairs_f[$i]="${SETOPT_KEYS_F[$i]}${setopt_pairs[${SETOPT_KEYS[$i]}]}"
+  done
+  kb_nav setopt_pairs_f $((LINES-2)) $((COLUMNS-2))
+}
+
+draw_select() {
+  local -i optkey_idx=$1
+  win_ctx_op 'push'
+  : "2,${SAGITTAL},$((LINES-2)),$((SAGITTAL-1)),${SETOPT_KEYS[$1]}_A,0,0"
+  win_ctx_op 'set' "2,${SAGITTAL},$((LINES-2)),$((SAGITTAL-1)),setopt_pairs_f,0,0"
+  draw_window
+  kb_nav $((LINES-4)) $((SAGITTAL-3))
 }
 
 kb_nav() {
   local key
   local -n ref=$1
-  local -ir rows=${#ref[@]}
   local -ir dim_y=$2
   local -ir dim_x=$3
-  local -r white_sp=$(echoes '\x20' $dim_x)
   local -ir lim_y_lo=$(((LINES-dim_y>>1)+1))
   local -ir lim_y_hi=$((lim_y_lo+dim_y-1))
+  local -ir len=${#ref[@]}
+  local -n idx=win_ctx[idx]
+  local -r white_sp=$(echoes '\x20' $dim_x)
   local -a curs
-  local -i row=0
   curs_store curs
-  printf '\xAF \x9B7m%s\x1B8\x9BB' "${ref[$row]}"
-  for ((i=0;++i<rows;)); do
+  printf '\xAF \x9B7m%s\x1B8\x9BB' "${ref[$idx]}"
+  for ((i=0;++i<len;)); do
     ((i==dim_y)) && break
     printf "\x1B7  ${ref[$i]}\x1B8\x9BB"
   done
@@ -269,7 +292,7 @@ kb_nav() {
       # UP
       k|$'\x9BA')
         # If non-zero index
-        ((row)) && {
+        ((idx)) && {
           # If cursor on first line of window
           (($(get_line)==lim_y_lo)) && {
             # Erasing whole window is easier without relative index references
@@ -278,23 +301,23 @@ kb_nav() {
             for ((i=0;i++<dim_y;));{ printf "${white_sp}\x1B8\x9BB\x1B7";}
             curs_load curs
             # Print highlighted selection on first line
-            printf '\xAF \x9B7m%s\x1B8\x9BB' "${ref[$((--row))]}"
+            printf '\xAF \x9B7m%s\x1B8\x9BB' "${ref[$((--idx))]}"
             # Print remaining lines
             for ((i=0;++i<dim_y;)); do
-              printf '\x1B7  %s\x1B8\x9BB' "${ref[$((row+i))]}"
+              printf '\x1B7  %s\x1B8\x9BB' "${ref[$((idx+i))]}"
             done
             curs_load curs
           } || {
           # Else remove highlight on current selection before printing next
-            printf '  %s\x1B8\x9BA\x1B7' "${ref[$((row--))]}"
-            printf '\xAF \x9B7m%s\x1B8' "${ref[$row]}"
+            printf '  %s\x1B8\x9BA\x1B7' "${ref[$((idx--))]}"
+            printf '\xAF \x9B7m%s\x1B8' "${ref[$idx]}"
           }
         }
       ;;
       # DOWN
       j|$'\x9BB')
         # If index less than total elements
-        ((row+1<rows)) && {
+        ((idx+1<len)) && {
           # If cursor on last line of window
           (($(get_line)==lim_y_hi)) && {
             # Erasing window
@@ -302,22 +325,22 @@ kb_nav() {
             for ((i=0;i++<dim_y;));{ printf "${white_sp}\x1B8\x9BB\x1B7";}
             curs_load curs
             # Print preceding lines
-            for ((i=row-dim_y+2;i<row+1;i++)); do
+            for ((i=idx-dim_y+2;i<idx+1;i++)); do
               printf '\x1B7  %s\x1B8\x9BB' "${ref[$i]}"
             done
             # Print highlighted selection on last line
-            printf '\x1B7\xAF \x9B7m%s\x1B8' "${ref[$((++row))]}"
+            printf '\x1B7\xAF \x9B7m%s\x1B8' "${ref[$((++idx))]}"
           } || {
           # Else remove highlight on current selection before printing next
-            printf '  %s\x1B8\x9BB\x1B7' "${ref[$((row++))]}"
-            printf '\xAF \x9B7m%s\x1B8' "${ref[$row]}"
+            printf '  %s\x1B8\x9BB\x1B7' "${ref[$((idx++))]}"
+            printf '\xAF \x9B7m%s\x1B8' "${ref[$idx]}"
           }
         }
       ;;
       # ENTER
       $'\n') 
-        printf '  \x9B7m%s\x1B8\x1B7' "${ref[$row]}"
-        draw_select $row
+        printf '  \x9B7m%s\x1B8\x1B7' "${ref[$idx]}"
+        draw_select $idx
       ;;
       # SPACE
       $'\x20') echo 'space';;
@@ -334,54 +357,54 @@ kb_nav() {
         case ${key} in
           $'\x9B1~') # HOME
             # Reprint options similar to when entering function
-            printf '\xAF \x9B7m%s\x1B8\x9BB' "${ref[$((row=0))]}"
-            for ((i=row;++i<rows;)); do
+            printf '\xAF \x9B7m%s\x1B8\x9BB' "${ref[$((idx=0))]}"
+            for ((i=idx;++i<len;)); do
               ((i==dim_y)) && break
               printf "\x1B7  ${ref[$i]}\x1B8\x9BB"
             done && curs_load curs
           ;;
           $'\x9B4~') # END
-            ((rows>dim_y)) && ((row=rows-dim_y-1)) || ((row=-1))
-            for ((row;++row<rows;)); do
+            ((len>dim_y)) && ((idx=len-dim_y-1)) || ((idx=-1))
+            for ((idx;++idx<len;)); do
               (($(get_line)>lim_y_hi)) && break
-              printf "\x1B7  ${ref[$row]}\x1B8\x9BB"
+              printf "\x1B7  ${ref[$idx]}\x1B8\x9BB"
             done
-            printf '\x1B8\xAF \x9B7m%s\x1B8' "${ref[$((--row))]}"
+            printf '\x1B8\xAF \x9B7m%s\x1B8' "${ref[$((--idx))]}"
           ;;
           $'\x9B5~') # PGUP
             # If scroll margins are within first page
             # Reprint options similar to when entering function
-            ((row<dim_y+1)) && {
-              printf '\xAF \x9B7m%s\x1B8\x9BB' "${ref[$((row=0))]}"
-              for ((i=row;++i<rows;)); do
+            ((idx<dim_y+1)) && {
+              printf '\xAF \x9B7m%s\x1B8\x9BB' "${ref[$((idx=0))]}"
+              for ((i=idx;++i<len;)); do
                 ((i==dim_y)) && break
                 printf "\x1B7  ${ref[$i]}\x1B8\x9BB"
               done && curs_load curs
             } || {
             # Else print from offset onward
-              ((row-=dim_y))
-              printf '\xAF \x9B7m%s\x1B8\x9BB' "${ref[$row]}"
+              ((idx-=dim_y))
+              printf '\xAF \x9B7m%s\x1B8\x9BB' "${ref[$idx]}"
               for ((i=0;++i<dim_y;)); do
-                printf "\x1B7  ${ref[$((row+i))]}\x1B8\x9BB"
+                printf "\x1B7  ${ref[$((idx+i))]}\x1B8\x9BB"
               done && curs_load curs
             }
           ;;
           $'\x9B6~') # PGDOWN
             # If scroll margins are within last page
             # Reprint options similar to when END key is pressed
-            ((row>rows-dim_y-2)) && {
-              ((rows>dim_y)) && ((row=rows-dim_y-1)) || ((row=-1))
-              for ((row;++row<rows;)); do
+            ((idx>len-dim_y-2)) && {
+              ((len>dim_y)) && ((idx=len-dim_y-1)) || ((idx=-1))
+              for ((idx;++idx<len;)); do
                 (($(get_line)>lim_y_hi)) && break
-                printf "\x1B7  ${ref[$row]}\x1B8\x9BB"
+                printf "\x1B7  ${ref[$idx]}\x1B8\x9BB"
               done
-              printf '\x1B8\xAF \x9B7m%s\x1B8' "${ref[$((--row))]}"
+              printf '\x1B8\xAF \x9B7m%s\x1B8' "${ref[$((--idx))]}"
             } || {
             # Else print add offset and print to sum
-              ((row+=dim_y))
+              ((idx+=dim_y))
               for ((i=dim_y;--i>0;)); do
-                printf "\x1B7  ${ref[$((row-i))]}\x1B8\x9BB"
-              done && printf '\x1B7\xAF \x9B7m%s\x1B8' "${ref[$row]}"
+                printf "\x1B7  ${ref[$((idx-i))]}\x1B8\x9BB"
+              done && printf '\x1B7\xAF \x9B7m%s\x1B8' "${ref[$idx]}"
             }
           ;;
         esac
@@ -390,32 +413,14 @@ kb_nav() {
   }
 }
 
-keymap_handler() {
-  printf '%s\n' "${KEYMAP_FILES[@]}"
-  local -a curs
-  #curs_store curs
-  #draw_window 2 $SAGITTAL $((LINES-2)) $((SAGITTAL-1))
-  #kb_nav
-  #printf "%s" ${keymaps[@]}
-  #curs_load curs
-}
-
-locale_handler() {
-  for ((i=0;i++<${#SUPPORTED_LOCALES[@]};)); do
-    printf '%s\n' "${SUPPORTED_LOCALES[$i]}"
-  done
-}
-
 main() {
   trap 'reset_console' EXIT
-  trap 'get_console_size; draw_window' SIGWINCH
+  #trap 'get_console_size; draw_window' SIGWINCH
   trap 'exit_prompt' SIGINT
   [[ $1 == -d ]] && test_size || set_console
   parse_files
   display_init
-  draw_menu
+  draw_main
   nap 2
-  #keymap_handler
-  #exit_prompt
 }
 main "$@"
--- sandbox.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-
-[[ ${0%/*} == ${0} ]] && dir='.' || dir=${0%/*}
-echo "$dir"
-echo $(dirname $0)
